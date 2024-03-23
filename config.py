ec1972d31c64c5fc7a26387fffca1bdc6e5f2bf7
saralameri <72477930+saralameri@users.noreply.github.com>
2024-03-23 04:16:11 +0400

    indentation

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
-import os 
+import os
 from dotenv import load_dotenv
+
 
 class Config:
     def __init__(self):
@@ -8,7 +9,7 @@ class Config:
 
     def get(self, key):
         return os.environ.get(key)
-    
+
     def set(self, key, value):
         feedback = os.environ[key] = value
         return feedback
--- main.py
+++ main.py
@@ -3,17 +3,11 @@ from io import BytesIO
 import json
 import psycopg
 import time
-# from configparser import ConfigParser
-
-from config import Config 
+from config import Config
 
 config = Config()
 DB_CONNECTION = config.get("DB_CONNECTION")
 API_CALL = config.get("API_CALL")
-
-# configFile = "config.ini"
-# config = ConfigParser()
-# config.read(configFile)
 
 buffer = BytesIO()
 curl = pycurl.Curl()
@@ -25,64 +19,67 @@ curl.setopt(curl.WRITEDATA, buffer)
 
 
 while True:
-    with psycopg.connect(DB_CONNECTION) as connection:
-        # emptying the buffer
-        buffer.seek(0)
-        buffer.truncate()
-        # executes the HTTP request
-        curl.perform()
-        # retrieves the HTTP response code of the request that was just performed.
-        response_code = curl.getinfo(curl.RESPONSE_CODE)
-        response_body = buffer.getvalue().decode("utf-8")
-        # If the response code indicates a successful request (e.g., 200)
-        if response_code == 200:
-            data = json.loads(response_body)
-        else:
-            # handle if the response code indicates an error
-            print(f"Failed to fetch data. Response code: {response_code}")
-            break
+    connection = psycopg.connect(DB_CONNECTION)
+    # emptying the buffer
+    buffer.seek(0)
+    buffer.truncate()
+    # executes the HTTP request
+    curl.perform()
+    # retrieves the HTTP response code of the request that was just performed.
+    response_code = curl.getinfo(curl.RESPONSE_CODE)
+    response_body = buffer.getvalue().decode("utf-8")
 
-        # Open a cursor to perform database operations
-        with connection.cursor() as cursor:
-            # collect the insert SQL query requirements
-            col = ""
-            val = list()
+    # handle if the response code indicates an error
+    if response_code != 200:
+        print(f"Failed to fetch data. Response code: {response_code}")
+        break
+    # If the response code indicates a successful request
+    data = json.loads(response_body)
 
-            for key in data:
-                if isinstance(data[key], dict) and key != "weather":
-                    temp = f"{key}_" + f", {key}_".join(data[key].keys())
-                    col = ", ".join([col, temp])
-                    val.extend(data[key].values())
-                elif key != "weather":
-                    col = ", ".join([col, key])
-                    val.append(data[key])
+    # Open a cursor to perform database operations
+    cursor = connection.cursor()
 
-            col = col.strip(", ")
-            sql_insert = "INSERT INTO api_res ({}) VALUES ({});".format(
-                col, ", ".join("%s" for _ in col.split(","))
+    # collect the insert SQL query requirements
+    col = ""
+    val = list()
+
+    for key in data:
+        if isinstance(data[key], dict) and key != "weather":
+            temp = f"{key}_" + f", {key}_".join(data[key].keys())
+            col = ", ".join([col, temp])
+            val.extend(data[key].values())
+        elif key != "weather":
+            col = ", ".join([col, key])
+            val.append(data[key])
+
+    col = col.strip(", ")
+    sql_insert = "INSERT INTO api_res ({}) VALUES ({});".format(
+        col, ", ".join("%s" for _ in col.split(","))
+    )
+    cursor.execute(sql_insert, val)
+
+    PK = cursor.execute(
+        "SELECT res_id FROM api_res ORDER BY res_id DESC LIMIT 1"
+    ).fetchone()[0]
+
+    for inst in data["weather"]:
+        weatherID = inst["id"]
+        weatherPK = cursor.execute(
+            f"select id from weather where id = {weatherID}"
+        ).fetchall()
+        if len(weatherPK) == 0:
+            sql_insert = "INSERT INTO weather ({}) VALUES ({});".format(
+                (", ".join(inst.keys())), ", ".join("%s" for _ in inst)
             )
-            cursor.execute(sql_insert, val)
+            cursor.execute(sql_insert, list(inst.values()))
 
-            PK = cursor.execute(
-                "SELECT res_id FROM api_res ORDER BY res_id DESC LIMIT 1"
-            ).fetchone()[0]
+        sql_insert = "INSERT INTO res_weather_relation (res_id, weather_id) VALUES ({}, {});".format(
+            PK, inst["id"]
+        )
+        cursor.execute(sql_insert)
 
-            for inst in data["weather"]:
-                weatherID = inst["id"]
-                weatherPK = cursor.execute(
-                    f"select id from weather where id = {weatherID}"
-                ).fetchall()
-                if len(weatherPK) == 0:
-                    sql_insert = "INSERT INTO weather ({}) VALUES ({});".format(
-                        (", ".join(inst.keys())), ", ".join("%s" for _ in inst)
-                    )
-                    cursor.execute(sql_insert, list(inst.values()))
+    cursor.close()
+    connection.commit()
+    connection.close()
 
-                sql_insert = "INSERT INTO res_weather_relation (res_id, weather_id) VALUES ({}, {});".format(
-                    PK, inst["id"]
-                )
-                cursor.execute(sql_insert)
-
-            connection.commit()
-            connection.close()
     time.sleep(300)  # 300 sec = 5 min
